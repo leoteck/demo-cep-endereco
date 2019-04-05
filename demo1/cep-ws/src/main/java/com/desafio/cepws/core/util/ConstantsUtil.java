adbcd2775b6e942e309c639335cb7a2b57fa0633
Leonardo Rodrigues <leoteck@gmail.com>
2019-04-04 23:56:47 -0300

    [ENDERECO-API] Ajustes  de mensagens, testes unitários e remoção de código comentado.

--- demo1/cep-ws/src/main/java/com/desafio/cepws/core/util/ConstantsUtil.java
+++ demo1/cep-ws/src/main/java/com/desafio/cepws/core/util/ConstantsUtil.java
@@ -3,4 +3,7 @@ package com.desafio.cepws.core.util;
 public class ConstantsUtil {
     public static final int TOTAL_DIGITOS_CEP = 8;
     public static final int LENGTH_STRING_DIGITOS_CEP = 7;
+
+    public static final String MESSAGE_CEP_NAO_ENCONTRADO = "CEP não encontrado!";
+    public static final String MESSAGE_CEP_INVALIDO = "CEP inválido!";
 }
